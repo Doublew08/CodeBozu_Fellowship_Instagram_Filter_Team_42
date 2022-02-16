070d279e28f8c64700f0709e89ec7686af04658c
MoiMohamed <dodo2062006@gmail.com>
2022-02-16 17:23:58 +0200

    add

---
+++ vintage,sepia,sharp.py
@@ -0,0 +1,38 @@
+import cv2 as cv
+import numpy as np
+import matplotlib.pyplot as pil
+
+img = cv.imread("Bozu.jpg", 1)
+
+def vintage(img):
+    kx = cv.getGaussianKernel(img.shape[0] , 200)
+    ky = cv.getGaussianKernel(img.shape[1] , 200)
+
+    kernel = kx * ky.T
+    mask = 255 * kernel / np.linalg.norm(kernel)
+    
+    img[:, :, 0] = img[:, :, 0] * mask
+    img[:, :, 1] = img[:, :, 2] * mask
+    img[:, :, 2] = img[:, :, 2] * mask
+
+    return img
+
+def sepia(img):
+    kernel_sepia = np.matrix([[0.393, 0.769, 0.189],
+                             [0.349, 0.686, 0.168],
+                             [0.272, 0.534, 0.131]])
+
+    result = cv.transform(img, kernel_sepia)
+    
+    return result
+
+def sharp(img):
+    kernel_sharp = np.array([[-1, -1, -1], [-1, 9.5, -1], [-1, -1, -1]])
+    result = cv.filter2D(img, -1, kernel_sharp)
+    
+    return result
+
+result = vintage(img)
+
+pil.imshow(result)
+pil.show()
